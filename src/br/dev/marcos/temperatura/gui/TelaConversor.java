a889c2d76a2a2e4ef37bda31890942660a2e04d8
Ryannvs <ryannvs89@gmail.com>
2025-04-28 09:14:55 -0300

    Código finalizado e comentado

--- src/br/dev/marcos/temperatura/gui/TelaConversor.java
+++ src/br/dev/marcos/temperatura/gui/TelaConversor.java
@@ -1,47 +1,114 @@
 package br.dev.marcos.temperatura.gui;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import br.dev.marcos.temperatura.model.Temperatura;
+
 public class TelaConversor {
-	
+	private JLabel lblCelsius;
+	private JTextField textCelsius;
+	private JButton btnKelvin;
+	private JButton btnFahreinheit;
+	private JLabel lblResultado;
+	private JLabel lblMensagemErro;
+
 	public void criarTelaConversor() {
 
+		// Criando a tela onde será colocado as informações
 		JFrame tela = new JFrame();
-		tela.setSize(700, 500);
+		tela.setSize(530, 350);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setTitle("Conversor de Temperatura");
 		tela.setLocationRelativeTo(null);
 		tela.setLayout(null);
 
+		// Texto do Celsius
 		JLabel lblCelsius = new JLabel();
 		lblCelsius.setText("Temperatura em graus celsius:");
 		lblCelsius.setBounds(20, 10, 200, 60);
 
+		// botão para converter para Kelvin
 		JButton btnKelvin = new JButton();
 		btnKelvin.setText("Kelvin");
 		btnKelvin.setBounds(20, 110, 230, 30);
 
+		// botão para converter para Fahreinheit
 		JButton btnFahreinheit = new JButton();
 		btnFahreinheit.setText("Fahreinheit");
-		btnFahreinheit.setBounds(270, 110, 230, 30);;
-
-		JLabel lblResultado = new JLabel();
+		btnFahreinheit.setBounds(270, 110, 230, 30);
+		;
 		
-
+		//Local para mostrar o resultado
+		JLabel lblResultado = new JLabel();
+		lblResultado.setBounds(230, 160, 230, 30);
+		;
+		
+		//Local para mostrar a mensagem de erro
 		JLabel lblMensagemErro = new JLabel();
-
+		lblMensagemErro.setBounds(190, 200, 230, 30);
+		lblMensagemErro.setForeground(Color.red);
+		
+		//Caixa de texto para inserir os Celsius
 		JTextField textCelsius = new JTextField();
 		textCelsius.setBounds(20, 60, 480, 30);
+
+		btnKelvin.addActionListener(new ActionListener() {
+
+			@Override
+			// Condição para a inserção de apenas números
+			public void actionPerformed(ActionEvent e) {
+				String txtCelsius = textCelsius.getText();
+				try {
+					double celsius = Double.parseDouble(txtCelsius);
+
+					Temperatura kelvin = new Temperatura();
+					kelvin.setCelsius(celsius);
+					celsius = kelvin.converterParaKelvin();
+					lblResultado.setText(celsius + " KELVIN");
+					// Formatação do texto
+				} catch (NumberFormatException e2) {
+					lblMensagemErro.setText("Insira apenas números!");
+
+				}
+			}
+		});
+
+		btnFahreinheit.addActionListener(new ActionListener() {
+			@Override
+			// Condição para a inserção de apenas números no botão
+			public void actionPerformed(ActionEvent e) {
+				String txtCelsius = textCelsius.getText();
+				try {
+					double celsius = Double.parseDouble(txtCelsius);
+
+					Temperatura fahreinheit = new Temperatura();
+					celsius = fahreinheit.converterParaFahreinheit();
+					lblResultado.setText(celsius + " FAREHINHEIT");
+					// Formatação do texto
+				} catch (NumberFormatException e2) {
+					lblMensagemErro.setText("Insira apenas números!");
+
+				}
+			}
+		});
 		
+		//Adicionando os conteúdos na tela
 		tela.getContentPane().add(lblCelsius);
 		tela.getContentPane().add(btnKelvin);
 		tela.getContentPane().add(btnFahreinheit);
-		
+		tela.getContentPane().add(lblResultado);
+		tela.getContentPane().add(lblMensagemErro);
 		tela.getContentPane().add(textCelsius);
 		
+		//Deixando a tela visível 
 		tela.setVisible(true);
 
 	}
