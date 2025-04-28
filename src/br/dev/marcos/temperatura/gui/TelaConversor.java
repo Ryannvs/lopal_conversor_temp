a645068fcd6c2b7062505c0655334dfbf976e130
Ryannvs <ryannvs89@gmail.com>
2025-04-28 08:16:49 -0300

    Fazendo a tela

--- src/br/dev/marcos/temperatura/gui/TelaConversor.java
+++ src/br/dev/marcos/temperatura/gui/TelaConversor.java
@@ -10,24 +10,38 @@ public class TelaConversor {
 	public void criarTelaConversor() {
 
 		JFrame tela = new JFrame();
-		tela.setSize(700, 700);
+		tela.setSize(700, 500);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setTitle("Conversor de Temperatura");
 		tela.setLocationRelativeTo(null);
 		tela.setLayout(null);
 
 		JLabel lblCelsius = new JLabel();
+		lblCelsius.setText("Temperatura em graus celsius:");
+		lblCelsius.setBounds(20, 10, 200, 60);
 
 		JButton btnKelvin = new JButton();
+		btnKelvin.setText("Kelvin");
+		btnKelvin.setBounds(20, 110, 230, 30);
 
 		JButton btnFahreinheit = new JButton();
+		btnFahreinheit.setText("Fahreinheit");
+		btnFahreinheit.setBounds(270, 110, 230, 30);;
 
 		JLabel lblResultado = new JLabel();
+		
 
 		JLabel lblMensagemErro = new JLabel();
 
 		JTextField textCelsius = new JTextField();
-
+		textCelsius.setBounds(20, 60, 480, 30);
+		
+		tela.getContentPane().add(lblCelsius);
+		tela.getContentPane().add(btnKelvin);
+		tela.getContentPane().add(btnFahreinheit);
+		
+		tela.getContentPane().add(textCelsius);
+		
 		tela.setVisible(true);
 
 	}
